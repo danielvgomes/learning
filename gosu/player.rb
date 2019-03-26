813f8db266842ba0af36c9c101e2b1fa8080ad6c
danielvgomes <danielvgomes@gmail.com>
2019-03-26 12:18:20 -0300

    tutorial gosu

---
+++ gosu/player.rb
@@ -0,0 +1,40 @@
+class Player
+  def initialize
+    @image = Gosu::Image.new("media/starfighter.bmp")
+    @x = @y = @vel_x = @vel_y = @angle = 0.0
+    @x2 = @y2 = 0.0
+    @score = 0
+  end
+
+  def warp(x, y)
+    @x, @y = x, y
+  end
+  
+  def turn_left
+    @angle -= 4.5
+  end
+  
+  def turn_right
+    @angle += 4.5
+  end
+  
+  def accelerate
+    @vel_x += Gosu.offset_x(@angle, 0.5)
+    @vel_y += Gosu.offset_y(@angle, 0.5)
+  end
+  
+  def move
+    @x2 += @vel_x
+    @y2 += @vel_y
+    #@x %= 640
+    #@y %= 480
+    
+    @vel_x *= 0.95
+    @vel_y *= 0.95
+    p @x2.to_s + " " + @y2.to_s
+  end
+
+  def draw
+    @image.draw_rot(@x, @y, 1, @angle)
+  end
+end
---
+++ gosu/test_player.rb
@@ -0,0 +1,54 @@
+
+require 'gosu'
+require_relative "player"
+
+class Tutorial < Gosu::Window
+
+module ZOrder
+  BACKGROUND, STARS, PLAYER, UI = *0..3
+end
+
+
+
+
+
+  def initialize
+    super 640, 480
+    self.caption = "Tutorial Game"
+    @font = Gosu::Font.new(20)
+
+    @background_image = Gosu::Image.new("media/space.png", :tileable => true)
+
+    @player = Player.new
+    @player.warp(320, 240)
+  end
+
+  def update
+    if Gosu.button_down? Gosu::KB_LEFT or Gosu::button_down? Gosu::GP_LEFT
+      @player.turn_left
+    end
+    if Gosu.button_down? Gosu::KB_RIGHT or Gosu::button_down? Gosu::GP_RIGHT
+      @player.turn_right
+    end
+    if Gosu.button_down? Gosu::KB_UP or Gosu::button_down? Gosu::GP_BUTTON_0
+      @player.accelerate
+    end
+    @player.move
+  end
+
+  def draw
+    @player.draw
+    @background_image.draw(0, 0, ZOrder::BACKGROUND)
+    @font.draw("FPS: #{Gosu.fps}", 10, 10, ZOrder::UI, 1.0, 1.0, Gosu::Color::YELLOW)
+  end
+
+  def button_down(id)
+    if id == Gosu::KB_ESCAPE
+      close
+    else
+      super
+    end
+  end
+end
+
+Tutorial.new.show
