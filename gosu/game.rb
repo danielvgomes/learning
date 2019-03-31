4f70b8410e537bc5b0f3876909ca8227e6f546f0
danielvgomes <danielvgomes@gmail.com>
2019-03-30 21:20:14 -0300

    limpano

--- gosu/game.rb
+++ gosu/game.rb
@@ -12,39 +12,30 @@ end
     self.caption = "Tutorial Game"
     @cursor = Gosu::Image.new('media/cursor.png')
     @font = Gosu::Font.new(20)
-    @nextX = @nextY = 0
-    @angle = 0
 
     @background_image = Gosu::Image.new("media/bg.bmp", :tileable => true)
 
     @player = Player.new
     @player.warp(320, 240)
+    @nextX = @player.x
+    @nextY = @player.y
+    @angle = 0
   end
 
   def update
-#    if Gosu.button_down? Gosu::KB_LEFT or Gosu::button_down? Gosu::GP_LEFT
-#      @player.turn_left
-#    end
-#    if Gosu.button_down? Gosu::KB_RIGHT or Gosu::button_down? Gosu::GP_RIGHT
-#      @player.turn_right
-#    end
-#    if Gosu.button_down? Gosu::KB_UP or Gosu::button_down? Gosu::GP_BUTTON_0
-#      @player.accelerate
-#    end
-#    @player.move
 
-     @nextX = mouse_x
-     @nextY = mouse_y
+    #if (@player.x - @nextX).abs > 10 && (@player.y - @nextY) > 10
+    #  @player.accelerate(@angle)
+    #end
 
-     @angle = Gosu.angle(@player.x, @player.y, mouse_x, mouse_y)
-    
-     if @player.x != @nextX && @player.y != @nextY
-       @player.accelerate(@angle)
-     end
+    @player.move
 
-     @player.move
-
-    
+    if Gosu.button_down? Gosu::MS_LEFT
+      @nextX = mouse_x
+      @nextY = mouse_y
+      @angle = Gosu.angle(@player.x, @player.y, mouse_x, mouse_y)
+      @player.accelerate(@angle)
+    end
   end
 
   def draw
@@ -55,9 +46,9 @@ end
     # @background_image.draw(400, 400, ZOrder::BACKGROUND)
     @background_image.draw(400, 0, ZOrder::BACKGROUND)
     @font.draw("FPS: #{Gosu.fps}", 10, 10, ZOrder::UI, 1.0, 1.0, Gosu::Color::YELLOW)
-    @font.draw("Mouse: #{mouse_x}, #{mouse_y}", 100, 10, ZOrder::UI, 1.0, 1.0, Gosu::Color::YELLOW)
-    @font.draw("Player: #{@player.x}, #{@player.y}", 300, 10, ZOrder::UI, 1.0, 1.0, Gosu::Color::YELLOW)
-    @font.draw("Angle to Mouse: #{@angle}", 10, 30, ZOrder::UI, 1.0, 1.0, Gosu::Color::YELLOW)
+    @font.draw("Mouse: #{mouse_x}, #{mouse_y}", 10, 30, ZOrder::UI, 1.0, 1.0, Gosu::Color::YELLOW)
+    @font.draw("Wilber: #{'%.1f' % @player.x}, #{'%.1f' % @player.y}", 10, 50, ZOrder::UI, 1.0, 1.0, Gosu::Color::YELLOW)
+    @font.draw("Angle: #{'%.1f' % @angle}", 10, 70, ZOrder::UI, 1.0, 1.0, Gosu::Color::YELLOW)
 
 
   end
