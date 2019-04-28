ea27adbebef137545fa2f218b0a3956352bb3a34
danielvgomes <danielvgomes@gmail.com>
2019-04-28 20:41:48 -0300

    teclas e mouse funcionando, segfault ainda presente

--- opengl/010/win.cpp
+++ opengl/010/win.cpp
@@ -4,12 +4,24 @@ win::win()
 {
 	width = 800;
 	height = 600;
+
+	for (size_t i = 0; i < 1024; i++)
+	{
+		keys[i] = 0;
+
+	}
 }
 
 win::win(GLint window_width, GLint window_height)
 {
 	width = window_width;
 	height = window_height;
+
+	for (size_t i = 0; i < 1024; i++)
+	{
+		keys[i] = 0;
+
+	}
 }
 
 int win::initialize()
@@ -45,6 +57,9 @@ int win::initialize()
 	// set context for GLEW to use
 	glfwMakeContextCurrent(main_window);
 
+	// handle key + mouse input
+	create_callbacks();
+
 	// allow modern extension features
 	glewExperimental = GL_TRUE;
 
@@ -62,8 +77,64 @@ int win::initialize()
 	// setup viewport size
 	glViewport(0, 0, buffer_width, buffer_height);
 
+	glfwSetWindowUserPointer(main_window, this);
+
 	return 0;
 }
+
+void win::create_callbacks()
+{
+	glfwSetKeyCallback(main_window, handle_keys);
+	glfwSetCursorPosCallback(main_window, handle_mouse);
+}
+
+
+void win::handle_keys(GLFWwindow* window, int key, int code, int action, int mode)
+{
+	win* the_window = static_cast<win*>(glfwGetWindowUserPointer(window));
+
+	if(key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, GL_TRUE);
+	}
+
+	if(key >= 0 && key < 1024)
+	{
+		if(action == GLFW_PRESS)
+		{
+			the_window->keys[key] = true;
+			printf("Pressed: %d\n", key);
+		}
+		else if (action == GLFW_RELEASE)
+		{
+			the_window->keys[key] = false;
+			printf("Released: %d\n", key);
+		}
+	}
+
+
+
+}
+
+void win::handle_mouse(GLFWwindow* window, double x_pos, double y_pos)
+	{
+		win* the_window = static_cast<win*>(glfwGetWindowUserPointer(window));
+		if (the_window->mouse_first_moved)
+		{
+			the_window->last_x = x_pos;
+			the_window->last_y = y_pos;
+			the_window->mouse_first_moved = false;
+		}
+
+		the_window->x_change = x_pos - the_window->last_x;
+		the_window->y_change = the_window->last_y - y_pos;
+
+		the_window->last_x = x_pos;
+		the_window->last_y = y_pos;
+
+		printf("x:%f, y:%f\n", the_window->x_change, the_window->y_change);
+	}
+
 
 
 win::~win()
--- opengl/010/win.h
+++ opengl/010/win.h
@@ -25,4 +25,17 @@ class win
 
 		GLint width, height;
 		GLint buffer_width, buffer_height;
+
+		bool keys[1024];
+
+		GLfloat last_x;
+		GLfloat last_y;
+		GLfloat x_change;
+		GLfloat y_change;
+		bool mouse_first_moved;
+
+		void create_callbacks();
+
+		static void handle_keys(GLFWwindow* window, int key, int code, int action, int mode);
+		static void handle_mouse(GLFWwindow* window, double x_pos, double y_pos);
 };
