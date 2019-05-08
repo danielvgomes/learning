782ae3ee956fdee62d3b9757da8d2f7e4f227c8a
danielvgomes <danielvgomes@gmail.com>
2019-05-07 23:55:13 -0300

    finalmente - o problema era no source do shader

--- opengl/SB_001/sb_001_05.cpp
+++ opengl/SB_001/sb_001_05.cpp
@@ -13,9 +13,12 @@ time_t timer;
 long past_time;
 
 GLuint rendering_program;
+GLuint program;
 GLuint vertex_array_object;
+GLuint vertex_shader;
+GLuint fragment_shader;
 
-GLuint compile_shaders(void);
+void compile_shaders(void);
 GLenum err;
 void startup(void);
 void shutdown(void);
@@ -40,7 +43,7 @@ int main() {
 	glfwMakeContextCurrent(window);
 	glewExperimental = GL_TRUE;
 	glewInit();
-	startup();
+	compile_shaders();
 
 	const GLubyte* renderer = glGetString(GL_RENDERER);
 	const GLubyte* version = glGetString(GL_VERSION);
@@ -51,7 +54,6 @@ int main() {
 
 		frames++;
 		counter++;
-		// printf("frames: %ld\n", frames);
 
 		if (time(&timer) > past_time)
 		{
@@ -67,12 +69,11 @@ int main() {
 		glClearBufferfv(GL_COLOR, 0, color);
 
 		// use program
-		glUseProgram(rendering_program);
+		glUseProgram(program);
 
 		// draw one thing
 		glDrawArrays(GL_POINTS, 0, 1);
 		glPointSize(abs(100*(float)sin(counter*PI/180)));
-		// printf("lollkkooo %f\n", 100*(float)sin(counter*PI/180));
 		
 		glfwPollEvents(); // se nao, nao da nem pra fechar a janela (input handler bitch), kkk
 
@@ -84,57 +85,55 @@ int main() {
 			if (err == GL_INVALID_ENUM) { printf("INVALID_ENUM\n"); }
 			if (err == GL_INVALID_VALUE) { printf("INVALID_VALUE\n"); }
 			if (err == GL_INVALID_OPERATION) { printf("INVALID_OPERATION\n"); }
-			if (err == GL_INVALID_FRAMEBUFFER_OPERATION) { printf("INVALD_FRAMEBUFFER_OPERATION\n"); }
+			if (err == GL_INVALID_FRAMEBUFFER_OPERATION) { printf("INVALID_FRAMEBUFFER_OPERATION\n"); }
 			if (err == GL_OUT_OF_MEMORY) { printf("GL_OUT_OF_MEMORY\n"); }
 			if (err == GL_STACK_UNDERFLOW) { printf("GL_STACK_UNDERFLOW\n"); }
 			if (err == GL_STACK_OVERFLOW) { printf("GL_STACK_OVERFLOW\n"); }
 		}
 	}
 
-	shutdown(); // sera que e aqui que poe isso?
 	glfwTerminate();
 	return 0;
 }
 
-GLuint compile_shaders(void)
+void compile_shaders(void)
 {
 	printf("compile shaders function\n");
-	GLuint vertex_shader;
-	GLuint fragment_shader;
-	GLuint program;
 
 	// source code for vertex shader
-	static const GLchar * vertex_shader_source[] =
-	{
-		"#version 430 core								\n"
-		"										\n"
-		"void main(void)								\n"
-		"{										\n"
-		"	gl_Position = vec4(0.0, 0.0, 0.5, 1.0);					\n"
-		"}										\n"
-	};
+	static const char * vertex_shader_source = "						\n\
+		#version 430 core								\n\
+												\n\
+		void main()									\n\
+		{										\n\
+			gl_Position = vec4(0.0, 0.0, 0.5, 1.0);					\n\
+		}";
 
 	// source code for fragment shader
-	static const GLchar * fragment_shader_source[] =
-	{
-		"#version 430 core								\n"
-		"										\n"
-		"out vec4 color;								\n"
-		"										\n"
-		"void main(void)								\n"
-		"{										\n"
-		"	color = vec4(0.0, 0.8, 1.0, 1.0);					\n"
-		"}										\n"
-	};
+	static const char * fragment_shader_source = "						\n\
+		#version 430 core								\n\
+												\n\
+		out vec4 color;									\n\
+												\n\
+		void main()									\n\
+		{										\n\
+			color = vec4(0.0, 0.8, 1.0, 1.0);					\n\
+		}";
+
+	const GLchar* vs_code[1];
+	const GLchar* fs_code[1];
+
+	vs_code[0] = vertex_shader_source;
+	fs_code[0] = fragment_shader_source;
 
 	// create and compile vertex shader
 	vertex_shader = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(vertex_shader, 1, vertex_shader_source, NULL);
+	glShaderSource(vertex_shader, 1, vs_code, NULL);
 	glCompileShader(vertex_shader);
 
 	// create and compile fragment shader
 	fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fragment_shader, 1, fragment_shader_source, NULL);
+	glShaderSource(fragment_shader, 1, fs_code, NULL);
 	glCompileShader(fragment_shader);
 
 	// create program, attach shaders to it, and link it
@@ -146,26 +145,38 @@ GLuint compile_shaders(void)
 		printf(" deu ruim: shader\n");
 	}
 
-
-
 	glAttachShader(program, vertex_shader);
 	glAttachShader(program, fragment_shader);
 	glLinkProgram(program);
+
+	GLint is_linked;
+	glGetProgramiv(program, GL_LINK_STATUS, (GLint *)&is_linked);
+	if (is_linked == GL_FALSE)
+	{
+		printf("deu ruim: linkar\n");
+
+		GLint max_length;
+		glGetProgramiv(program, GL_INFO_LOG_LENGTH, &max_length);
+		if (max_length > 0)
+		{
+			char *p_link_info_log = new char[max_length];
+			glGetProgramInfoLog(program, max_length, &max_length, p_link_info_log);
+			printf("deu ruim, log: %s\n", p_link_info_log);
+			delete [] p_link_info_log;
+		}
+	}
 
 	// delete the shaders as the program has them now
 	glDeleteShader(vertex_shader);
 	glDeleteShader(fragment_shader);
 
-	return program;
-
 }
-
 
 void startup()
 {
-	rendering_program = compile_shaders();
-	glGenVertexArrays(1, &vertex_array_object); // testar remover isso
-	glBindVertexArray(vertex_array_object); // testar remover isso
+	compile_shaders();
+	glGenVertexArrays(1, &vertex_array_object);
+	glBindVertexArray(vertex_array_object);
 }
 
 void shutdown()
@@ -174,4 +185,3 @@ void shutdown()
 	glDeleteProgram(rendering_program);
 	glDeleteVertexArrays(1, &vertex_array_object);
 }
-
