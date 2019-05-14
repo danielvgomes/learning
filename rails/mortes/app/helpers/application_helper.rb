c0eba2ca593533d3b062ab3bcab96c8a224f8c90
danielvgomes <danielvgomes@gmail.com>
2019-05-13 22:19:44 -0300

    tentando um bagulho no rails

---
+++ rails/mortes/app/helpers/application_helper.rb
@@ -0,0 +1,24 @@
+module ApplicationHelper
+    def hello_world
+     "Hello, World"
+  end
+
+    def series_a
+      {
+        "1993": 1030,
+        "1994": "5000",
+        "2000": "12"
+      }
+    end
+
+    def series_b
+      {
+        "1993": 134,
+        "2005": "42",
+        "2006": "13"
+      }
+    end
+
+
+
+end
