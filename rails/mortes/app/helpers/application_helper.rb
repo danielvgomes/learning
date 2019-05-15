f0d6776cb9656bb4783c5dff23ab99378d84b755
danielvgomes <danielvgomes@gmail.com>
2019-05-14 23:38:18 -0300

    mds agora vai rails

--- rails/mortes/app/helpers/application_helper.rb
+++ rails/mortes/app/helpers/application_helper.rb
@@ -1,26 +1,26 @@
 module ApplicationHelper
-
+ 
   require 'csv'
   require 'json'
-
+ 
     def hello_world
      "Hello, World"
   end
-
-
+ 
     def series_a
-      data = File.open('../mortes/data/FEM2010.csv').read
-      CSV.parse(data).to_json
+      # data = File.open('../mortes/data/FEM2010.csv').read
+      # CSV.parse(data).to_json
     end
-
-    def series_b
+ 
+    def fem_branca_e14
       {
-        "1993": 134,
-        "2005": "42",
-        "2006": "13"
+        "2010": 13942,
+        "2011": 14133,
+        "2012": 13826,
+        "2013": 13804,
+        "2014": 13309,
+        "2015": 13371,
+        "2016": 13514
       }
     end
-
-
-
 end
