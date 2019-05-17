cc116ccd40c42a2b950ae8437c3314a8a6a8b366
danielvgomes <danielvgomes@gmail.com>
2019-05-17 00:17:12 -0300

    ae, agora ta rolando rails

--- rails/persefoni/app/helpers/application_helper.rb
+++ rails/persefoni/app/helpers/application_helper.rb
@@ -20,32 +20,73 @@ module ApplicationHelper
 #      }
 #    end
 
-    def series_b
-      lol = {"test" => []}
-      lol["test"].push({"Ano" => "12", "moscas" => "1"})
-      lol["test"].push({"Ano" => "15", "moscas" => "13"})
-      
-      lol2 = []
-      lol2.push({"2001" => 1})
-      lol2.push({"2002" => 7})
+#    def series_b
+#      lol = {"test" => []}
+#      lol["test"].push({"Ano" => "12", "moscas" => "1"})
+#      lol["test"].push({"Ano" => "15", "moscas" => "13"})
+#      
+#      lol2 = []
+#      lol2.push({"2001" => 1})
+#      lol2.push({"2002" => 7})
+#
+#      za = {"test" => []}
+#      za["test"].push({"lol" => 23})
+#      za["test"].push({"lol1" => 223})
+#
+#      ba = []
+#      ba.push("2001" => 1)
+#      ba.push("2007" => 15)
+#
+#      be = { "2001" => []}
+#      be["2001"].push("test" => 14)
+#      be["2001"].push("gwlo" => 15)
+#      be["2001"].push("fef" => 17)
+#
+#      # car = {"2001" => 13, "2002" => 14}
+#      car = {}
+##      car["2001"] = 20
+#      car["2002"] = 120
+#      car["2003"] = 90
+#      car["2004"] = 80
+#      car["2005"] = 112
+#      car["2006"] = 415
+#      car["2007"] = 300
+#
+#      return car
+#    end
 
-      za = {"test" => []}
-      za["test"].push({"lol" => 23})
-      za["test"].push({"lol1" => 223})
+class Dictionary
+  def initialize
+    @hash = {}
+  end
 
-      ba = []
-      ba.push("2001" => 1)
-      ba.push("2007" => 15)
-
-      be = { "2001" => []}
-      be["2001"].push("test" => 14)
-      be["2001"].push("gwlo" => 15)
-      be["2001"].push("fef" => 17)
-
-      car = {"2001" => 13, "2002" => 14}
-      car["2005"] = 23
-      return car
-  
-
+  def add(defs)
+    defs.each do |word, definition|
+      @hash[word] = definition
     end
+  end
+    def entries
+     @hash
+  end
+
+  def keywords
+     @hash.keys
+  end
+end
+
+def series_b
+  data1 = Dictionary.new
+  Dir["../persefoni/data/*.csv"].select { |f| File.file? f
+                        data = File.open(f).read
+                        data = CSV.parse(data, headers: true)
+
+                        data1.add((f.gsub(/[^0-9]/, '')) => data[0]["Branca"].to_i)
+  }
+
+  return data1.entries
+
+end
+
+    
+
 end
--- rails/persefoni/data/test.rb
+++ rails/persefoni/data/test.rb
@@ -3,14 +3,81 @@ require 'csv'
 data = File.open('FEM2010.csv').read
 lol = CSV.parse(data, headers: true)
 
+class Dictionary
+  def initialize
+    @hash = {}
+  end
+
+  def add(defs)
+    defs.each do |word, definition|
+      @hash[word] = definition
+    end
+  end
+    def entries
+     @hash
+  end
+
+  def keywords
+     @hash.keys
+  end
+end
+
+p lol[0]["Categoria CID-10"] # diabetes
+p lol[1]["Categoria CID-10"] # parkinson
+p lol[2]["Categoria CID-10"] # infarto
+p lol[3]["Categoria CID-10"] # estrangulamento
+p lol[4]["Categoria CID-10"] # arma de fogo
+p lol[5]["Categoria CID-10"] # calibre maior
+p lol[6]["Categoria CID-10"] # disparo
+p lol[7]["Categoria CID-10"] # chama
+p lol[8]["Categoria CID-10"] # cortante
+p lol[9]["Categoria CID-10"] # contundente
+p lol[10]["Categoria CID-10"] # forca corporal
+p lol[11]["Categoria CID-10"] # nao especificado
+p lol[12]["Categoria CID-10"] # intervencao legal
+#p lol[13]["Categoria CID-10"] # Total
+#p lol[14]["Categoria CID-10"]
+#p lol[15]["Categoria CID-10"]
+
+p "##"
+
+p lol[0]["Parda"]
+# p lol[1]["Parda"]
+p lol[2]["Parda"]
+#p lol[3]["Parda"]
+#p lol[4]["Parda"]
+#p lol[5]["Parda"]
+p lol[6]["Parda"]
+#p lol[7]["Parda"]
+p lol[8]["Parda"]
+#p lol[9]["Parda"]
+#p lol[10]["Parda"]
+#p lol[11]["Parda"]
+#p lol[12]["Parda"]
 
 
-p lol[2]["Categoria CID-10"]
+
+data1 = Dictionary.new
 
 Dir["*.csv"].select { |f| File.file? f
   data = File.open(f).read
   data = CSV.parse(data, headers: true)
-  p data
+
+#  data1 = 
+# data1 = [f.gsub(/[^0-9]/, '')] = data[0]["Branca"]
+  data1.add((f.gsub(/[^0-9]/, '')) => data[0]["Branca"].to_i)
+
+
 }
 
+p "eita porra duvido"
+p "#################"
+p data1.entries
 
+
+car = {}
+
+car["2001"] = 20
+car["2002"] = 199
+
+p car
