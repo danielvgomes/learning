2ad19e7fac7152e313d506e3d3f0fd5558ab6569
Daniel Gomes <danielvgomes@gmail.com>
2019-07-13 20:34:37 -0300

    taficani bunitinhu

--- godot/005/Main.cs
+++ godot/005/Main.cs
@@ -5,6 +5,10 @@ public class Main : Node2D
 {
 	[Export]
 	PackedScene ship;
+	
+	Camera2D myCam = new Camera2D();
+	RigidBody2D s;
+
 	// Ship s;
 	
 	// Called when the node enters the scene tree for the first time.
@@ -14,7 +18,27 @@ public class Main : Node2D
 		GetViewport().Connect("size_changed", this, nameof(onViewportSizeChanged));
 		
 		Log.debug = true;
-		var s = ship.Instance() as RigidBody2D;
+		s = ship.Instance() as RigidBody2D;
+		s.SetPosition(new Vector2(10000f, 10000f));
+		// var cam = (Camera2D)GetNode("Camera2D");
+		// cam.MakeCurrent();
+		// Node2D menode = GetNode<Node2D>("Node2D");
+		s.AddChild(myCam);
+		myCam.MakeCurrent();
+		// var p = (CPUParticles2D)GetNode("ParallaxBackground/ParallaxLayer/CPUParticles2D");
+		// Log.p("p emmiting: " + p.Emitting);
+		// p.Emitting = true;
+		// Log.p(myCam.AnchorMode);
+
+		myCam.SetZoom(new Vector2(1.5f, 1.5f));
+		myCam.DragMarginLeft = 0;
+		myCam.DragMarginRight = 0;
+		myCam.DragMarginTop = 0;
+		myCam.DragMarginBottom = 0;
+		// myCam.SmoothingEnabled = true;
+		
+		// Log.p(cam.Position);
+		// Log.p("esa cameras e' corete: " + cam.Current);
 		// s = new Ship();
 		AddChild(s);
 		Log.p("AddChild(s.ship)");
@@ -42,6 +66,7 @@ public class Main : Node2D
 		{
 			Log.p("Click");
 			Log.p(GetViewport().GetMousePosition().x + ", " + GetViewport().GetMousePosition().y);
+			Log.p("agrak q saok eaks x: " + s.Position.x + ", " + s.Position.y);
 		}
 	}
 	
