04b8a67a90a0e9c84af152b92b0d239a510685d6
Daniel Gomes <danielvgomes@gmail.com>
2019-07-13 23:09:40 -0300

    shooting wip

--- godot/005/Ship.cs
+++ godot/005/Ship.cs
@@ -6,11 +6,23 @@ public class Ship : RigidBody2D
 	private Vector2 thrust = new Vector2(0, 250);
 	private float torque = 10000;
 	private float rotationDir;
-	private Bullet b;
+	private Vector2 shootingPosition;
+	
+	[Export]
+	PackedScene bullet;
 	
 	public Ship()
 	{
+		// bulletScene = (PackedScene)ResourceLoader.Load("res://Bullet.tscn");
 	}
+	
+	public void Shoot()
+	{
+		Log.p("Shoot()");
+		Log.p(GetNode<Position2D>("Position2D").GlobalPosition);
+		shootingPosition = GetNode<Position2D>("Position2D").GlobalPosition;
+	}
+	
 	
 	public override void _IntegrateForces(Physics2DDirectBodyState state)
 	{
@@ -38,7 +50,7 @@ public class Ship : RigidBody2D
 		}
 		if (Input.IsActionPressed("ui_home"))
 		{
-			
+			Shoot();
 		}
 		
 		SetAppliedTorque(rotationDir * torque);
